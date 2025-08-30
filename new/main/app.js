010433eb79f89d7a17325260ee432fb1c4776116
andresmazo6 <135562856+andresmazo6@users.noreply.github.com>
2025-08-30 16:19:53 -0500

    Update app.js

--- new/main/app.js
+++ new/main/app.js
@@ -1,5 +1,5 @@
 // URL del backend en Apps Script (reemplaza TU_URL cuando lo tengas)
-const GAS_URL = "https://script.google.com/macros/s/TU_URL/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbzMVVYQcIW2e7cNU9iGK96cR84_8-dqgpdO80rSkziQh-0_Yfu41u4HokxFDIL-K3Iewg/exec";
 
 async function testPing() {
   try {
